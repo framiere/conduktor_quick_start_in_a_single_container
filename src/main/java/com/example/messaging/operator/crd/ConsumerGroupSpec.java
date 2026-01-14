cb02678bded40f461664664b0eef4ec699a66020
Florent Ramiere <florent@ramiere.com>
2026-01-14 20:23:21 +0100

    fix: remove @Builder from ConsumerGroupSpec to enable no-args constructor

--- src/main/java/com/example/messaging/operator/crd/ConsumerGroupSpec.java
+++ src/main/java/com/example/messaging/operator/crd/ConsumerGroupSpec.java
@@ -4,12 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import io.fabric8.generator.annotation.Required;
 import io.sundr.builder.annotations.Buildable;
-import lombok.Builder;
 import lombok.Data;
 
 @Buildable(editableEnabled = false)
 @Data
-@Builder(toBuilder = true)
 public class ConsumerGroupSpec {
 
     @JsonProperty("serviceRef")
